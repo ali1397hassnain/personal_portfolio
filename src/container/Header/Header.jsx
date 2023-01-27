f3d328ec96d21f36935d845c9b9fb31a9447c5c0
ali1397hassnain <40492735+ali1397hassnain@users.noreply.github.com>
2023-01-27 15:40:27 +0500

    my portfolio changes

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -58,7 +58,7 @@ const Header = () => (
       whileInView={scaleVariants.whileInView}
       className="app__header-circles"
     >
-      {[images.next, images.redux, images.sanity].map((circle, index) => (
+      {[images.react, images.next, images.sanity].map((circle, index) => (
         <div className="circle-cmp app__flex" key={`circle-${index}`}>
           <img src={circle} alt="profile_bg" />
         </div>
